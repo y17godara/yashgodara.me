b2e8fe3808d5db56ce248ff856c1cb440186030e
Yash Godara <yash17godara@gmail.com>
2023-06-16 23:55:46 +0530

    Resume Link Fixed

--- components/Navigations/TopNavbar.tsx
+++ components/Navigations/TopNavbar.tsx
@@ -39,12 +39,18 @@ const TopNavbar = () => {
             >
               <SiTwitter size={20} />
             </a> */}
-            <button 
-            title="Download Resume"
-            className="bg-gray-400 border-b-2 border-gray-900 hover:border-gray-900 hover:bg-gray-900 hover:text-white text-gray-800 py-2 px-4 rounded inline-flex items-center">
-              <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+            <a
+              href="/resume.pdf"
+              download="resume.pdf"
+              title="Download Resume"
+              className="bg-gray-400 border-b-2 border-gray-900 hover:border-gray-900 hover:bg-gray-900 hover:text-white text-gray-800 py-2 px-4 rounded inline-flex items-center"
+            >
+              <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
               <span>Resume</span>
-            </button>
+            </a>
+
           </div>
         </div>
       </section>
